4115b78b0c7e806717cc78542ce957fed7aafed9
Qi <at20s@icloud.com>
2017-10-13 19:07:01 +0800

    update

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,6 +1,12 @@
 public class NBody {
 
-	public double readRadius(String path){
-		
+	public static double readRadius(String path){
+		System.out.println("E:\\sp17\\skeleton-sp17\\proj0\\data\\planets.txt");
+		In in = new In("E:\\sp17\\skeleton-sp17\\proj0\\data\\planets.txt");
+		double radius = 0;
+		while(!in.isEmpty()) {
+			radius = in.readDouble();		
+		}
+		return radius;
 	}
 }
