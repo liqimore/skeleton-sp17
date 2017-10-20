00924751a2cf8bc5d6ca65acc49321ca099bd885
Jackrabbit <at20s@icloud.com>
2017-10-20 11:17:41 +0800

    committee one

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,12 +1,19 @@
 public class NBody {
 
 	public static double readRadius(String path){
-		System.out.println("E:\\sp17\\skeleton-sp17\\proj0\\data\\planets.txt");
-		In in = new In("E:\\sp17\\skeleton-sp17\\proj0\\data\\planets.txt");
-		double radius = 0;
-		while(!in.isEmpty()) {
-			radius = in.readDouble();		
+		In in = new In(path);
+		String[] radius = in.readAllLines();
+		//String to double
+		return Double.valueOf(radius[1]).doubleValue();
+	}
+
+	public static Planet[] ReadPlanets(String path){
+		In in = new In(path);
+		int numOfPlanet = in.readInt();
+		String[] lines = in.readAllLines();
+		String[] Planet = new Planet();
+		for (int i = 2; i < numOfPlanet + 2; i++) {
+			double info[] = In.readDoubles(lines[i]);
 		}
-		return radius;
 	}
 }
