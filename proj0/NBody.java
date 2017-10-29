8d82fe2f9eb4809bc94a6bd6e77122e1f217f8dc
Jackrabbit <at20s@icloud.com>
2017-10-30 01:03:47 +0800

    commit

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -6,14 +6,21 @@ public class NBody {
 		//String to double
 		return Double.valueOf(radius[1]).doubleValue();
 	}
-
-	public static Planet[] ReadPlanets(String path){
+	
+	public static Planet[] readPlanets(String path){
 		In in = new In(path);
 		int numOfPlanet = in.readInt();
 		String[] lines = in.readAllLines();
-		String[] Planet = new Planet();
+		Planet[] arrayOfPlanet;
 		for (int i = 2; i < numOfPlanet + 2; i++) {
-			double info[] = In.readDoubles(lines[i]);
+			String info[] = In.readDoubles(lines[i]);
+			for (int j = 0; j < 6; j++) {
+				arrayOfPlanet[j] = new Planet(Double.parseDouble(info[0]), Double.parseDouble(info[1])
+											  Double.parseDouble(info[2]), Double.parseDouble(info[3])
+											  Double.parseDouble(info[4]), info[5]);
+			}
 		}
+		return arrayOfPlanet;
 	}
+	
 }
